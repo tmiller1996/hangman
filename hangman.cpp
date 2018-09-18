a527b0b0434d3d67b236607fa3216b40ec289d24
Tim Miller <t.r.miller0823@gmail.com>
2018-09-18 02:15:55 -0400

    Implemented the rest of the game logic

--- hangman.cpp
+++ hangman.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <algorithm>
 #include <string>
+#include <sstream>
 
 template<typename Iter, typename RandomGenerator>
 Iter random_select(Iter start, Iter end, RandomGenerator &rng){
@@ -39,13 +40,17 @@ protected:
 
 	std::string randomWord() {
 		auto it = random_select(std::begin(lines), std::end(lines), rng);
-		lowerWord(*it);
 		return *it;
 	}
 
-	bool wordValid(const std::string &word){
+	bool validWord(const std::string &word){
 		if(ends_with(word, "'s")){
 			return false;
+		}
+		for(const auto &c : word){
+			if(c < 'a' || c > 'z'){
+				return false;
+			}
 		}
 		return true;
 	}
@@ -61,15 +66,15 @@ public:
 		std::ifstream ifile(path);
 		std::string word;
 		while(std::getline(ifile, word)){
-			lines.push_back(word);
+			lowerWord(word);
+			if(validWord(word)){
+				lines.push_back(word);
+			}
 		}
 	}
 
 	std::string getWord() {
 		auto word = randomWord();
-		while(!wordValid(word)){
-			word = randomWord();
-		}
 		return word;
 	}
 };
@@ -77,7 +82,7 @@ public:
 class Hangman {
 protected:
 	std::string word;
-	uint32_t lives;
+	int lives;
 	std::vector<char> guessed;
 	std::vector<char> misses;
 
@@ -92,11 +97,44 @@ protected:
 	}
 
 	bool lost() {
-		return lives == 0;
+		return lives < 0;
 	}
 
 	bool validGuess(char guess){
 		return guess >= 'a' && guess <= 'z';
+	}
+
+	void handleGuess(char guess){
+		if(validGuess(guess)){
+			if(!contains(guessed, guess)){
+				if(contains(word, guess)){
+					guessed.push_back(guess);
+					std::cout << "Correct!" << std::endl;
+				}
+				else{
+					misses.push_back(guess);
+					--lives;
+					std::cout << "Wrong!" << std::endl;
+				}
+			}
+			else {
+				std::cout << "You already guessed '" << guess << "'" << std::endl;
+			}
+		}
+		else{
+			std::cout << "'" << guess << "' is not a valid guess" << std::endl;
+		}
+	}
+
+	void printWord() {
+		std::string display(word.size(), '_');
+		for(std::size_t i = 0; i < word.size(); ++i){
+			char c = word[i];
+			if(contains(guessed, c)){
+				display[i] = c;
+			}
+		}
+		std::cout << display << std::endl;
 	}
 
 public:
@@ -104,27 +142,95 @@ public:
 	}
 
 	void play(){
-		while(true){
-			char guess = std::cin.get();
-			if(guess != '\n'){
-				if(validGuess(guess)){
-					if(!contains(guessed, guess)){
-					}
-					else{
-						std::cout << "You already guessed '" << guess << "'" << std::endl;
-					}
+		std::string line;
+		while(!lost() && !won()){
+			printWord();
+			std::getline(std::cin, line);
+			if (line.size() == 0){
+				std::cout << "Expected input" << std::endl;
+			}
+			else if (line.size() == 1){
+				// got exactly 1 character
+				char guess = line[0];
+				handleGuess(guess);
+				if(won()){
+					std::cout << "You won!" << std::endl;
 				}
-				else{
-					std::cout << "'" << guess << "' is not a valid guess" << std::endl;
+				if(lost()){
+					std::cout << "Game over!" << std::endl;
 				}
+			}
+			else {
+				std::cout << "Please guess only 1 character" << std::endl;
 			}
 		}
 	}
 };
 
-int main(){
-	Dict dict("/usr/share/dict/words");
-	Hangman hm(dict.getWord(), 10);
+bool hasFlag(const std::vector<std::string> &args, const std::string &flag){
+	auto it = std::find(std::begin(args), std::end(args), flag);
+	return it != std::end(args);
+}
+
+std::pair<bool, std::string> hasFlagAndValue(const std::vector<std::string> &args, const std::string &flag){
+	std::pair<bool, std::string> result;
+	auto it = std::find(std::begin(args), std::end(args), flag);
+	if(it != std::end(args)){
+		std::advance(it, 1);
+		if(it != std::end(args)){
+			result = std::make_pair(true, *it);
+		}
+		else{
+			result = std::make_pair(false, "");
+		}
+	}
+	else{
+		result = std::make_pair(false, "");
+	}
+	return result;
+}
+
+void printUsage(){
+
+}
+
+int main(int argc, char **argv){
+	std::vector<std::string> args(argv, argv + argc);
+	std::cout << args[0] << std::endl;
+
+	bool help = hasFlag(args, "-h");
+	if(help){
+		printUsage();
+		exit(0);
+	}
+
+	int lives = 10;
+	auto livesResult = hasFlagAndValue(args, "-l");
+	if(livesResult.first){
+		try{
+			lives = std::stoi(livesResult.second);
+		}
+		catch(std::logic_error &err){
+			std::cout << "'-l' flag passed invalid integer" << std::endl;
+			exit(0);
+		}
+	}
+
+	std::string word;
+	auto wordResult = hasFlagAndValue(args, "-w");
+	if(wordResult.first){
+		word = wordResult.second;
+	}
+	if(word.size() == 0){
+		std::string file = "/usr/share/dict/words";
+		auto fileResult = hasFlagAndValue(args, "-f");
+		if(fileResult.first){
+			file = fileResult.second;
+		}
+		Dict dict(file);
+		word = dict.getWord();
+	}
+	Hangman hm(word, lives);
 	hm.play();
 	return 0;
 }
